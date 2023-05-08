ee2bc28b6f1c87f60224cd9e35461bcb5b33a8b5
Zane Miller <zanemiller148@u.boisestate.edu>
2023-05-08 14:57:52 -0600

    Final fix??

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -51,7 +51,7 @@ public class GettingStartedApplication {
             final var resultSet = statement.executeQuery("SELECT tick FROM table_timestamp_and_random_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getString("random_string"));
+                output.add("Read from DB: " + resultSet.getTimestamp("tick") + " " + resultSet.getString(2));
             }
 
             model.put("records", output);
